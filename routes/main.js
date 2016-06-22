ddb00caf09b61cc21c34443cd0c7f91c0d664abf
Jon Salter <jondsalter@gmail.com>
2016-06-22 13:03:34 -0700

    deploy attempt one

--- routes/main.js
+++ routes/main.js
@@ -11,6 +11,13 @@ module.exports = function(express, app) {
 		});
 	});
 
+	// HOMEPAGE with side menu bar
+	router.get('/sidebar', function(req, res){
+		res.render('index-two', {
+			pageName: "Homepage"
+		});
+	});
+
 	// Mount the router on the app
 	// All routes relative to '/'
 	app.use('/', router);
